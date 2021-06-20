5d969e30a8f575ee0d5db6be1e07497deaea21e0
Nadir Fejzic <nadirfejzo@gmail.com>
2021-06-20 14:25:39 +0200

    Change error message printed on incorrect usage

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     if args.len() != 2 {
         eprintln!("Incorrect usage!");
         eprintln!("Correct usage:");
-        eprintln!("\n\tcalculate \"expression\" ;\n\n\tWhere expression can contain decimal numbers, +, -, *, /, %, '(' and ')'");
+        eprintln!("\n\tcalculate \"expression\"\n");
+        eprintln!("\tWhere expression can contain decimal numbers, +, -, *, /, %, '(' and ')'");
         process::exit(1);
     }
 
@@ -17,7 +18,7 @@ fn main() {
         .get(1)
         .expect("Args has exactly 2 arguments at this point. Index 1 is valid");
 
-    let res = parse_expression(&&expression);
+    let res = parse_expression(&expression);
 
     match res {
         Ok(val) => println!("{} = {}", expression, val),
